a9f55a3f88a4212a50fda2403cb7e5f3e88121e0
ridickle7 <ridickle7@gmail.com>
2019-03-31 00:26:17 +0900

    YAPP plan.T - 2019.03.22(SUN) 00:23
    1. 메인 액티비티 작업 진행 (Fragment)
    2. (주의) ColorPickerView 작업 일부 진행 (brightness 등)

--- app/src/main/java/yapp14th/co/kr/myplant/ui/main/MainActivity.kt
+++ app/src/main/java/yapp14th/co/kr/myplant/ui/main/MainActivity.kt
@@ -1,15 +1,49 @@
 package yapp14th.co.kr.myplant.ui.main
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 import yapp14th.co.kr.myplant.R
 import yapp14th.co.kr.myplant.base.BaseActivity
+import yapp14th.co.kr.myplant.ui.main.tab1_home.HomeFragment
 
-class MainActivity: BaseActivity() {
+
+class MainActivity : BaseActivity() {
     override fun getLayoutRes() = R.layout.activity_main
     override fun getIsUseDataBinding() = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setToolbar(R.color.transparent)
+
+        var adapter = MainPagerAdapter(supportFragmentManager)
+
+        // 이부분에 fragment 추가하면 됨
+        adapter.init(arrayListOf<Fragment>(HomeFragment(), HomeFragment(), HomeFragment()))
+        vp_container.adapter = adapter
+
+        main_tab.setOnNavigationItemSelectedListener(object : BottomNavigationView.OnNavigationItemSelectedListener {
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                when (item.itemId) {
+                    R.id.action_home -> {
+                        vp_container.currentItem = 0
+                        return true
+                    }
+                    R.id.action_static -> {
+                        vp_container.currentItem = 1
+                        return true
+                    }
+                    R.id.action_mypage -> {
+                        vp_container.currentItem = 2
+                        return true
+                    }
+                }
+                return false
+            }
+        })
+
+        vp_container.currentItem = 0
     }
 }
---
+++ app/src/main/java/yapp14th/co/kr/myplant/ui/main/tab1_home/HomeViewModel.kt
@@ -0,0 +1,12 @@
+package yapp14th.co.kr.myplant.ui.main.tab1_home
+
+import android.app.Application
+import yapp14th.co.kr.myplant.base.BaseViewModel
+
+class HomeViewModel(app: Application) : BaseViewModel(app) {
+
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+}
