6ab94a92456464a7fe2e01f1bf2b0e91198e573e
Park JiHye <youo78@naver.com>
2019-03-30 21:31:36 +0900

    statistic fragment, layout, resources add

---
+++ app/src/main/java/yapp14th/co/kr/myplant/ui/main/tab2_statistic/StatisticViewModel.kt
@@ -0,0 +1,11 @@
+package yapp14th.co.kr.myplant.ui.main.tab2_statistic
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class StatisticViewModel(app: Application) : AndroidViewModel(app){
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+}
